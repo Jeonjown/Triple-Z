60396d5bc019c27fba8e5493c02d12f08ca52c33
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2024-12-19 18:32:38 +0800

    initialize react ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Triple ZZZZZZZZ</h1>
+      <h1>Triple Z - Test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
