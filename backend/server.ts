f3592ad7edf7ffa01ac6807467156e9b61f4803a
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2024-12-29 13:46:01 +0800

    cors updated

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,11 @@ const server = express();
 server.use(express.json());
 server.use(
   cors({
-    origin: ["http://localhost:5173", "https://triple-z.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://triple-z.vercel.app",
+      "https://triple-5e2z5dqbv-jeonjowns-projects.vercel.app",
+    ],
   })
 );
 
