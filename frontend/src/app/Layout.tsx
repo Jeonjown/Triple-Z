e8b35c9e8bee0309bee8cf7526e362a78f6705f6
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2025-02-10 18:00:59 +0800

    change z index icon

--- frontend/src/app/Layout.tsx
+++ frontend/src/app/Layout.tsx
@@ -19,7 +19,7 @@ export default function AdminSidebarLayout({
       {/* Sidebar Component */}
       <Toaster />
       <AppAdminSidebar />
-      <SidebarTrigger className="absolute top-24 z-50 md:hidden" />
+      <SidebarTrigger className="absolute top-24 z-40 md:hidden" />
       {/* Main Content */}
       <div className="mt-24 w-full">{children}</div>
     </SidebarProvider>
