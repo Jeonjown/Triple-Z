826e3fedc63784626c83fe875db1f0db7d0a3bd9
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2025-02-12 00:09:48 +0800

    header testing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -57,24 +57,25 @@ const Home = () => {
     <>
       {/* HEADER */}
       <section className="relative overflow-hidden">
-        <div className="relative flex min-h-screen w-full flex-col items-center text-center xl:flex-row xl:items-center">
-          {/* Background Image for Mobile */}
+        <div className="relative flex min-h-screen w-full flex-col items-center text-center lg:flex-row xl:items-center">
+          {/* Background Image for Mobile (shifted down for 1:1 ratio) */}
           <img
-            src="landing-mobile.webp"
+            src="paper-background-mobile.png"
             alt="event background mobile"
-            className="absolute inset-0 h-full w-full object-cover xl:hidden"
+            className="absolute h-full w-full object-cover object-[50%_50%] lg:top-40 xl:hidden"
           />
 
-          {/* Background Image for XL */}
+          {/* Background Image for XL (shifted right for 1:3 ratio) */}
+
           <img
-            src="landing-pc.webp"
+            src="paper-background.png"
             alt="event pictures"
-            className="absolute inset-0 hidden h-full w-full object-cover xl:block"
+            className="absolute left-48 hidden h-full w-full object-cover object-[0%_50%] xl:block"
           />
 
           {/* Content */}
           <div className="relative z-10 px-5 py-10 sm:p-8 lg:max-w-prose xl:ml-20 xl:mr-auto xl:-translate-y-10">
-            <h1 className="font-heading text-5xl font-light sm:text-6xl lg:mt-10 lg:text-7xl xl:mt-0">
+            <h1 className="font-heading text-5xl font-light sm:text-6xl lg:text-7xl xl:mt-0">
               Welcome to Triple Z
             </h1>
             <h2 className="mt-6 text-lg sm:mt-5 sm:text-xl">
@@ -90,6 +91,18 @@ const Home = () => {
             >
               Book Now!
             </Button>
+          </div>
+          <div className="z-10 flex w-full items-center justify-center">
+            <img
+              src="Triple-z-image.webp"
+              alt="event background mobile"
+              className="w-full max-w-sm md:top-10 md:max-w-lg lg:hidden lg:max-w-xl"
+            />
+            <img
+              src="Triple-z-image.webp"
+              alt="event background mobile"
+              className="hidden w-full max-w-2xl lg:flex"
+            />
           </div>
         </div>
       </section>
