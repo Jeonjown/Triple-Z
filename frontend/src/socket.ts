7b2b0c05b47278f45270013df68cddaee71e6be1
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2025-03-29 19:56:15 +0800

    rtest

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  import.meta.env.VITE_SOCKET_URL || "http://localhost:3000",
+  import.meta.env.VITE_API_URL || "http://localhost:3000",
   {
     withCredentials: true,
   },
