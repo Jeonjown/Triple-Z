bff79aa22893331d3369265686a6c0039b876bb0
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2025-03-29 19:45:40 +0800

    test

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,9 +1,8 @@
 import { io } from "socket.io-client";
 
-const endpoint =
-  import.meta.env.VITE_API_URL.replace(/^https:/, "wss:") ||
-  "ws://localhost:3000";
-
-export const socket = io(endpoint, {
-  withCredentials: true,
-});
+export const socket = io(
+  import.meta.env.VITE_SOCKET_URL || "http://localhost:3000",
+  {
+    withCredentials: true,
+  },
+);
