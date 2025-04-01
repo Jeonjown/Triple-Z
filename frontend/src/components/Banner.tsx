3bda2920396ee35626f91c6348e99cc9624853a8
Jeonjown <99638128+Jeonjown@users.noreply.github.com>
2025-04-01 19:36:04 +0800

    add landing page animation

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -1,41 +1,114 @@
+import { FC, useRef, useEffect } from "react";
+import { motion, useAnimation, useInView } from "motion/react";
 import { useGetEventReservationSettings } from "@/features/Events/hooks/useGetEventReservationSettings";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
 
-const Banner = () => {
+const Banner: FC = () => {
   const { data: settings } = useGetEventReservationSettings();
+  const controls = useAnimation();
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: true }); // Trigger animation only once when the element comes into view
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  const transition = {
+    duration: 1,
+    delay: 0.3,
+    ease: [0, 0.71, 0.2, 1.01],
+  };
+
   return (
-    <>
-      <section className="relative overflow-hidden">
-        <div className="relative flex min-h-screen w-full flex-col items-center text-center lg:flex-row xl:items-center">
-          {/* Background Image for Mobile (shifted down for 1:1 ratio) */}
-          <img
-            src="paper-background-mobile.png"
-            alt="event background mobile"
-            className="absolute h-full w-full object-cover object-[50%_50%] lg:top-40 xl:hidden"
-          />
+    <section className="relative overflow-hidden">
+      <div className="relative flex min-h-screen w-full flex-col items-center text-center lg:flex-row xl:items-center">
+        {/* Background Image for Mobile */}
+        <img
+          src="paper-background-mobile.png"
+          alt="event background mobile"
+          className="absolute h-full w-full object-cover object-[50%_50%] lg:top-40 xl:hidden"
+        />
 
-          {/* Background Image for XL (shifted right for 1:3 ratio) */}
+        {/* Background Image for XL */}
+        <img
+          src="paper-background.png"
+          alt="event pictures"
+          className="absolute left-48 hidden h-full w-full object-cover object-[0%_50%] xl:block"
+        />
 
-          <img
-            src="paper-background.png"
-            alt="event pictures"
-            className="absolute left-48 hidden h-full w-full object-cover object-[0%_50%] xl:block"
-          />
+        {/* Content */}
+        <div
+          ref={ref}
+          className="relative z-10 px-5 py-10 sm:p-8 lg:max-w-prose xl:ml-20 xl:mr-auto xl:-translate-y-10"
+        >
+          {/* Banner Header */}
+          <motion.h1
+            initial="hidden"
+            animate={controls}
+            variants={{
+              hidden: { opacity: 0, y: -50 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { ...transition, delay: 0.2 },
+              },
+            }}
+            className="font-heading text-5xl font-light sm:text-6xl lg:text-7xl xl:mt-0 xl:text-8xl"
+          >
+            Welcome to Triple Z
+          </motion.h1>
 
-          {/* Content */}
-          <div className="relative z-10 px-5 py-10 sm:p-8 lg:max-w-prose xl:ml-20 xl:mr-auto xl:-translate-y-10">
-            <h1 className="font-heading text-5xl font-light sm:text-6xl lg:text-7xl xl:mt-0 xl:text-8xl">
-              Welcome to Triple Z
-            </h1>
+          {/* Subheading */}
+          <motion.h2
+            initial="hidden"
+            animate={controls}
+            variants={{
+              hidden: { opacity: 0, y: -50 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { ...transition, delay: 0.4 },
+              },
+            }}
+            className="mt-6 text-lg sm:mt-5 sm:text-xl"
+          >
+            Discover the perfect space to celebrate life’s special occasions at
+            Triple Z Coffee. Reserve your spot today!
+          </motion.h2>
 
-            <h2 className="mt-6 text-lg sm:mt-5 sm:text-xl">
-              Discover the perfect space to celebrate life’s special occasions
-              at Triple Z Coffee. Reserve your spot today!
-            </h2>
-            <h3 className="my-5 text-base text-[#DFA593] sm:text-lg">
-              MON-FRI {settings?.openingHours} - {settings?.closingHours}
-            </h3>
+          {/* Opening Hours */}
+          <motion.h3
+            initial="hidden"
+            animate={controls}
+            variants={{
+              hidden: { opacity: 0, y: -50 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { ...transition, delay: 0.6 },
+              },
+            }}
+            className="my-5 text-base text-[#DFA593] sm:text-lg"
+          >
+            MON-FRI {settings?.openingHours} - {settings?.closingHours}
+          </motion.h3>
+
+          {/* Button */}
+          <motion.div
+            initial="hidden"
+            animate={controls}
+            variants={{
+              hidden: { opacity: 0, y: -50 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { ...transition, delay: 0.6 },
+              },
+            }}
+          >
             <Link to="/schedule">
               <Button
                 size="lg"
@@ -44,8 +117,21 @@ const Banner = () => {
                 Book Now!
               </Button>
             </Link>
-          </div>
-          <div className="z-10 flex w-full items-center justify-center">
+          </motion.div>
+        </div>
+
+        {/* Image Section */}
+        <div className="z-10 flex w-full items-center justify-center">
+          <motion.div
+            ref={ref}
+            initial="hidden"
+            animate={controls}
+            variants={{
+              hidden: { scale: 0.7 },
+              visible: { scale: 1.0, transition: transition },
+            }}
+            className="flex h-full w-full items-center justify-center"
+          >
             <img
               src="Triple-z-image.webp"
               alt="event background mobile"
@@ -56,10 +142,10 @@ const Banner = () => {
               alt="event background mobile"
               className="hidden w-full max-w-4xl lg:flex"
             />
-          </div>
+          </motion.div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
--- frontend/src/components/BeOurGuest.tsx
+++ frontend/src/components/BeOurGuest.tsx
@@ -1,3 +1,4 @@
+import { motion, useAnimation, useInView } from "motion/react"; // Import motion components
 import {
   MousePointerClick,
   Calendar,
@@ -6,41 +7,117 @@ import {
 } from "lucide-react";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
+import { useRef, useEffect } from "react";
 
 const BeOurGuest = () => {
+  const controls = useAnimation(); // Initialize animation controls
+  const ref = useRef(null); // Reference for the section element
+  const inView = useInView(ref, { once: true }); // Trigger animation once when the element is in view
+
+  // Trigger the animation when the section is in view
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  // Define transition details
+  const transition = {
+    duration: 0.8,
+    delay: 1,
+    ease: [0, 0.71, 0.2, 1.01],
+  };
+
   return (
-    <section className="relative min-h-screen px-4 py-10 lg:flex lg:items-center lg:justify-center">
+    <section
+      ref={ref}
+      className="relative min-h-screen px-4 py-10 lg:flex lg:items-center lg:justify-center"
+    >
       {/* Background Image */}
       {/* <img
         src="BookNow-bg-1.png"
         alt="background"
-        className="absolute inset-0 z-[-1] h-full w-full object-cover"
+        className="absolute inset-0 z-[-1] h-full w-full object-cover opacity-50"
       /> */}
 
       <div className="container mx-auto flex flex-col items-center gap-8 px-4 lg:flex-row lg:items-center">
         {/* Image Section */}
-        <div className="flex w-full justify-center lg:w-1/2">
+        <motion.div
+          initial="hidden"
+          animate={controls}
+          variants={{
+            hidden: { opacity: 0, y: -50 },
+            visible: { opacity: 1, y: 0, transition },
+          }}
+          className="flex w-full justify-center lg:w-1/2"
+        >
           <img
             src="be-our-guest-photo.png"
             alt="events image"
             className="w-64 sm:w-80 lg:w-auto"
           />
-        </div>
+        </motion.div>
 
         {/* Combined Text Section */}
-        <div className="w-full text-left lg:text-left">
-          {/* Header */}
-          <h2 className="text-center text-lg font-semibold text-[#B89A82] lg:text-left">
+        <motion.div
+          initial="hidden"
+          animate={controls}
+          variants={{
+            hidden: { opacity: 0, y: -50 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { ...transition, delay: 0.2 },
+            },
+          }}
+          className="w-full text-left lg:text-left"
+        >
+          <motion.h2
+            initial="hidden"
+            animate={controls}
+            variants={{
+              hidden: { opacity: 0, y: -50 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { ...transition, delay: 0.4 },
+              },
+            }}
+            className="text-center text-lg font-semibold text-[#B89A82] lg:text-left"
+          >
             BE OUR GUEST
-          </h2>
-          <p className="text-center font-heading text-5xl lg:text-left">
+          </motion.h2>
+          <motion.p
+            initial="hidden"
+            animate={controls}
+            variants={{
+              hidden: { opacity: 0, y: -50 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { ...transition, delay: 0.6 },
+              },
+            }}
+            className="text-center font-heading text-5xl lg:text-left"
+          >
             How to Book a Reservation
-          </p>
+          </motion.p>
 
-          {/* Instruction Items */}
           <div className="mt-4">
             {/* Instruction 1 */}
-            <div className="flex items-center space-x-2 p-4">
+            <motion.div
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { ...transition, delay: 0.8 },
+                },
+              }}
+              className="flex items-center space-x-2 p-4"
+            >
               <div className="flex h-16 w-16 flex-shrink-0 items-center justify-center rounded-full bg-white p-1 shadow-xl">
                 <MousePointerClick className="!size-9 text-primary" />
               </div>
@@ -53,9 +130,22 @@ const BeOurGuest = () => {
                   the "Events" section then click "Schedule."
                 </p>
               </div>
-            </div>
+            </motion.div>
+
             {/* Instruction 2 */}
-            <div className="flex items-center space-x-2 p-4">
+            <motion.div
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { ...transition, delay: 1.0 },
+                },
+              }}
+              className="flex items-center space-x-2 p-4"
+            >
               <div className="flex h-16 w-16 flex-shrink-0 items-center justify-center rounded-full bg-white p-1 shadow-xl">
                 <Calendar className="!size-9 text-primary" />
               </div>
@@ -68,9 +158,22 @@ const BeOurGuest = () => {
                   time.
                 </p>
               </div>
-            </div>
+            </motion.div>
+
             {/* Instruction 3 */}
-            <div className="flex items-center space-x-2 p-4">
+            <motion.div
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { ...transition, delay: 1.2 },
+                },
+              }}
+              className="flex items-center space-x-2 p-4"
+            >
               <div className="flex h-16 w-16 flex-shrink-0 items-center justify-center rounded-full bg-white p-1 shadow-xl">
                 <ReceiptText className="!size-9 text-primary" />
               </div>
@@ -83,9 +186,22 @@ const BeOurGuest = () => {
                   requests.
                 </p>
               </div>
-            </div>
+            </motion.div>
+
             {/* Instruction 4 */}
-            <div className="flex items-center space-x-2 p-4">
+            <motion.div
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { ...transition, delay: 1.4 },
+                },
+              }}
+              className="flex items-center space-x-2 p-4"
+            >
               <div className="flex h-16 w-16 flex-shrink-0 items-center justify-center rounded-full bg-white p-1 shadow-xl">
                 <CalendarCheck className="!size-9 text-primary" />
               </div>
@@ -98,18 +214,29 @@ const BeOurGuest = () => {
                   receive a confirmation email shortly!
                 </p>
               </div>
-            </div>
+            </motion.div>
           </div>
 
           {/* Call-to-action Button */}
-          <div>
+          <motion.div
+            initial="hidden"
+            animate={controls}
+            variants={{
+              hidden: { opacity: 0, y: -50 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { ...transition, delay: 1.6 },
+              },
+            }}
+          >
             <Link to="/schedule">
               <Button size="lg" className="mx-auto mt-10 block w-48">
                 Book Now!
               </Button>
             </Link>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
     </section>
   );
